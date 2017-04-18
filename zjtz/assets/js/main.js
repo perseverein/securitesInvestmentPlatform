16c243dad68ca1c24972a87af3fe65e6f12e2800
mia <1372773165@qq.com>
2017-04-18 17:33:10 +0800

    platform

---
+++ zjtz/assets/js/main.js
@@ -0,0 +1,10 @@
+
+    $(".form_date").datetimepicker({
+      format: "yyyy-mm-dd",
+      autoclose: true,
+      todayBtn: true,
+      language:'zh-CN',
+      pickerPosition:"bottom-left",
+      bootcssVer:3
+    });
+
